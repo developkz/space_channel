e7f000cd6bf2173719783b0f6e6f154d24b77ed7
[Alexandr] <[afominyh242@gmail.com]>
2022-05-03 17:19:58 +0700

    added saving image to folder

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import requests
+from pathlib import Path
+filepath = Path('images/HST-SM4.jpeg')
+filepath.parent.mkdir(parents=True, exist_ok=True)
 
-filename = 'HST-SM4.jpeg'
 url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
-
 response = requests.get(url)
 response.raise_for_status()
 
-with open(filename, 'wb') as file:
+with filepath.open('wb') as file:
     file.write(response.content)
