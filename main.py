1c60783013cf7e01c4b6f27d53b0b41533aaa2c6
[Alexandr] <[afominyh242@gmail.com]>
2022-05-12 22:50:05 +0700

    Functions annotations and code refactoring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
 import os
+import shutil
 from datetime import datetime
 from pathlib import Path
 from urllib.parse import urlparse
@@ -12,6 +13,7 @@ import telegram
 
 
 def download_file(url: str, path: str) -> None:
+    """Скачивает файл из адреса url в указанный путь path."""
     file_path = urlparse(url).path
     filename = os.path.basename(file_path)
     filesystem_path = Path(f'{path}/{filename}')
@@ -23,8 +25,9 @@ def download_file(url: str, path: str) -> None:
         file.write(response.content)
 
 
-def fetch_spacex_launch():
-    api_url = "https://api.spacexdata.com/v3/launches/65"
+def fetch_spacex_launch() -> list:
+    """Возвращает список ссылок на изображения с запуска SpaceX №67."""
+    api_url = "https://api.spacexdata.com/v3/launches/67"
 
     payload = {}
     headers = {}
@@ -36,12 +39,19 @@ def fetch_spacex_launch():
     return space_x_answer.json()['links']['flickr_images']
 
 
-def download_spacex_launch_images():
+def download_spacex_launch_images(count: int = 0) -> None:
+    """Загружает count изображений из запуска SpaceX."""
+    downloaded = 0
     for url in fetch_spacex_launch():
-        download_file(url, 'images')
+        if count > downloaded:
+            download_file(url, 'images')
+            downloaded += 1
+        else:
+            break
 
 
 def fetch_nasa_best_image(count: int = 1) -> list:
+    """Получает count ссылок на лучшие изображения дня, NASA 'A Picture Of the Day'."""
     api_url = "https://api.nasa.gov/planetary/apod"
 
     headers = {
@@ -52,12 +62,14 @@ def fetch_nasa_best_image(count: int = 1) -> list:
     return nasa_answer.json()
 
 
-def download_nasa_image(count: int = 1):
+def download_nasa_image(count: int = 1) -> None:
+    """Загружает count изображения из NASA 'A Picture Of the Day'."""
     for pic_of_day in fetch_nasa_best_image(count):
         download_file(pic_of_day['hdurl'] or pic_of_day['url'], 'images')
 
 
-def fetch_nasa_natural_earth():
+def fetch_nasa_natural_earth() -> list:
+    """Возвращает ссылки на изображения Natural Earth NASA."""
     api_url = 'https://api.nasa.gov/EPIC/api/natural/'
 
     headers = {
@@ -77,14 +89,16 @@ def fetch_nasa_natural_earth():
     return urls_to_images
 
 
-def download_nasa_natural_image(count: int = 1):
+def download_nasa_natural_image(count: int = 1) -> None:
+    """Загружает count ссылок из списка fetch_nasa_natural_earth() ссылок."""
     urls_list = fetch_nasa_natural_earth()
     # pprint(urls_list)
     for url in urls_list[:count]:
         download_file(url, 'images')
 
 
-async def post_telegram_image(path):
+async def post_telegram_image(path: str) -> None:
+    """Публикует изображения из директории в канал телеграм через телеграм бота."""
     for root, directory, files in os.walk(path):
         for file in files:
             bot.sendPhoto(photo=open(f'{path}/{file}', 'rb'),
@@ -92,16 +106,14 @@ async def post_telegram_image(path):
                           timeout=150)
 
 
-async def sleep_for_time(sleep):
+async def sleep_for_time(sleep: int) -> None:
+    """Sleep Time"""
     await asyncio.sleep(sleep)
 
 
-def move_images_to_archive(copy_from, copy_to):
-    source = copy_from
-    destination = copy_to
-    all_files = os.listdir(source)
-    for file in all_files:
-        Path(f'{source}/{file}').rename(f'{destination}/{file}')
+def delete_files(source: str) -> None:
+    """Удаляет директорию Path(source)"""
+    shutil.rmtree(source)
 
 
 if __name__ == '__main__':
@@ -117,22 +129,34 @@ if __name__ == '__main__':
     natural_images_count = int(os.getenv('NASA_NATURAL_IMAGES_TO_DOWNLOAD'))
     bot = telegram.Bot(telegram_token)
     images_path = Path('images/')
-    archive_path = Path('archive/')
     post_attempt = 1
 
     while True:
         print(f'Attempt {post_attempt}... STARTED!')
         print(f'Fetching and downloading images to "{images_path}/"')
+
         download_nasa_image(best_images_count)
         download_nasa_natural_image(natural_images_count)
+        download_spacex_launch_images(2)
+
         print('Images downloading... OK!')
         print('Posting images...')
+
         asyncio.run(post_telegram_image(images_path))
+
         print('Images Posting... OK!')
-        print(f'Archiving images to {archive_path}')
+
         asyncio.run(sleep_for_time(2))
-        move_images_to_archive(images_path, archive_path)
-        print(f'Images moved to archive "{archive_path}/"... OK!')
+
+        print(f'Images Deleting...')
+
+        asyncio.run(sleep_for_time(2))
+        delete_files(images_path)
+
+        print(f'Images deleting... OK!')
+
         post_attempt += 1
+
         print(f'Started sleep for {time_sleep} seconds...')
+
         asyncio.run(sleep_for_time(time_sleep))
