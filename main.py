351b12e02b3e7ff5eb71db042fe1fa66b5344203
[Alexandr] <[afominyh242@gmail.com]>
2022-05-26 00:49:33 +0700

    Refactored functions to get token inside __main__

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def fetch_spacex_launch(launch_number: int = 67) -> list:
     return space_x_answer.json()['links']['flickr_images']
 
 
-def download_spacex_launch_images(count: int = 0, launch_number: int = 0):
+def download_spacex_launch_images(count: int = 0, launch_number: int = 67):
     """Загружает count изображений из запуска launch_number SpaceX."""
     images_of_launch_urls_list = fetch_spacex_launch(launch_number)
     if count >= len(images_of_launch_urls_list):
@@ -46,12 +46,12 @@ def download_spacex_launch_images(count: int = 0, launch_number: int = 0):
             download_file(url, 'images')
 
 
-def fetch_nasa_best_image(count: int = 1):
+def fetch_nasa_best_image(token: str, count: int = 1):
     """Получает count ссылок на лучшие изображения дня, NASA 'A Picture Of the Day'."""
     api_url = "https://api.nasa.gov/planetary/apod"
 
     headers = {
-        'api_key': nasa_token,
+        'api_key': token,
         'count': count
     }
     try:
@@ -62,18 +62,18 @@ def fetch_nasa_best_image(count: int = 1):
         return None
 
 
-def download_nasa_image(count: int = 1) -> None:
+def download_nasa_image(token: str, count: int = 1) -> None:
     """Загружает count изображения из NASA 'A Picture Of the Day'."""
-    for pic_of_day in fetch_nasa_best_image(count):
+    for pic_of_day in fetch_nasa_best_image(token, count):
         download_file(pic_of_day['hdurl'] or pic_of_day['url'], 'images')
 
 
-def fetch_nasa_natural_earth() -> list:
+def fetch_nasa_natural_earth(token: str) -> list:
     """Возвращает ссылки на изображения Natural Earth NASA."""
     api_url = 'https://api.nasa.gov/EPIC/api/natural/'
 
     headers = {
-        'api_key': nasa_token,
+        'api_key': token,
     }
     nasa_answer = requests.request('GET', api_url, params=headers)
     urls_to_images = []
@@ -89,9 +89,9 @@ def fetch_nasa_natural_earth() -> list:
     return urls_to_images
 
 
-def download_nasa_natural_image(count: int = 1) -> None:
+def download_nasa_natural_image(token: str, count: int = 1):
     """Загружает count ссылок из списка fetch_nasa_natural_earth() ссылок."""
-    urls_list = fetch_nasa_natural_earth()
+    urls_list = fetch_nasa_natural_earth(token)
     # pprint(urls_list)
     for url in urls_list[:count]:
         download_file(url, 'images')
@@ -133,33 +133,33 @@ if __name__ == '__main__':
     post_attempt = 1
 
     # download_spacex_launch_images(23, 56)
-    pprint(fetch_nasa_best_image())
-    # while True:
-    #     print(f'Attempt {post_attempt}... STARTED!')
-    #     print(f'Fetching and downloading images to "{images_path}/"')
-    #
-    #     download_nasa_image(best_images_count)
-    #     download_nasa_natural_image(natural_images_count)
-    #     download_spacex_launch_images(spacex_launches_count)
-    #
-    #     print('Images downloading... OK!')
-    #     print('Images posting...')
-    #
-    #     asyncio.run(post_telegram_image(images_path))
-    #
-    #     print('Images Posting... OK!')
-    #
-    #     asyncio.run(sleep_for_time(2))
-    #
-    #     print(f'Images Deleting...')
-    #
-    #     asyncio.run(sleep_for_time(2))
-    #     delete_files(images_path)
-    #
-    #     print(f'Images Deleting... OK!')
-    #
-    #     post_attempt += 1
-    #
-    #     print(f'Started sleep for {time_sleep} seconds...')
-    #
-    #     asyncio.run(sleep_for_time(time_sleep))
+    # pprint(fetch_nasa_best_image())
+    while True:
+        print(f'Attempt {post_attempt}... STARTED!')
+        print(f'Fetching and downloading images to "{images_path}/"')
+
+        download_nasa_image(nasa_token, best_images_count)
+        download_nasa_natural_image(nasa_token, natural_images_count)
+        download_spacex_launch_images(spacex_launches_count, 66)
+
+        print('Images downloading... OK!')
+        print('Images posting...')
+
+        asyncio.run(post_telegram_image(images_path))
+
+        print('Images Posting... OK!')
+
+        asyncio.run(sleep_for_time(2))
+
+        print(f'Images Deleting...')
+
+        asyncio.run(sleep_for_time(2))
+        delete_files(images_path)
+
+        print(f'Images Deleting... OK!')
+
+        post_attempt += 1
+
+        print(f'Started sleep for {time_sleep} seconds...')
+
+        asyncio.run(sleep_for_time(time_sleep))
